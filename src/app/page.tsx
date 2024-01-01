81795c5e25286d8debe819f19f167db203e148a5
Hidayt Rahman <hidaytrahman@gmail.com>
2024-01-01 23:33:35 +0530

    feat: browser api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from '@/src/components/Header';
-import Card from '@/src/components/common/Card/Card';
 import Editor from '@/src/components/features/editor/Editor';
 import { browsers } from '@/src/data';
+import EntityBrowserItem from '../components/common/EntityBrowserItem/EntityBrowserItem';
 
 export default function Home() {
 	return (
@@ -12,7 +12,13 @@ export default function Home() {
 				<h3 className='text-2xl mb-3 font-bold leading-7 text-gray-900'>Browsers</h3>
 				<div className='flex gap-3'>
 					{browsers?.map((browser, index) => (
-						<Card key={index} title={browser.title} link={browser.link} info={browser.info} />
+						<EntityBrowserItem
+							key={index}
+							title={browser.title}
+							link={browser.link}
+							info={browser.info}
+							avatar={browser.avatar}
+						/>
 					))}
 				</div>
 			</section>
