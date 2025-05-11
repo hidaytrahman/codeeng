4e0b0076eb4c22dc8815a5be070d1e91151c618b
hidaytrahman <hidaytrahman@gmail.com>
2025-05-12 00:06:54 +0530

    feat: theme and mui refactor

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -9,14 +9,21 @@ const roboto = Roboto({
 });
 
 const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
+    // enable dark mode from system preference
+//   colorSchemes: {
+//     dark: true,
+//   },
   palette: {
     mode: "light",
     primary: {
-      main: "#ff5252",
-      light: "#ff8a80",
+      main: "#73946B",
+      light: "#9EBC8A",
+      dark: "#537D5D",
+    },
+    secondary: {
+      main: "#D2D0A0",
+      light: "#E4E1B5",
+      dark: "#B8B68A",
     },
   },
   typography: {
